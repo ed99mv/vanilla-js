c89e00ba74294c40807d73d7041ae096a9c7e001
Edward Medina <medinaedward988@gmail.comm>
2023-06-20 16:01:24 -0600

    actualizar fin

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,21 @@
 // Inserte el código aquí
+let arreglo=[1, 2, 3]
+let clonarArreglo =function () {
+    arregloClone=arreglo
+    console.log(arregloClone);
+    return arregloClone
+}
 
-
-
+clonarArreglo()
 // NO MODIFICAR
-export { clonarArreglo, clonarObjeto, sumar, saludar };
+// export { clonarArreglo, clonarObjeto, sumar, saludar };
+
+
+
+let casa = ("cuarto", "cocina", "baño")
+let clonarObjeto=function () {
+    casanueva=casa
+    console.log(casanueva);
+    return casanueva
+}
+clonarObjeto()
