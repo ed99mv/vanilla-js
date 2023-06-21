24ff060794de33106b55d5dbcf794f68d9d9f1ec
Edward Medina <medinaedward988@gmail.comm>
2023-06-21 15:56:57 -0600

    actualsj

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 // Inserte el código aquí
+function MhoraActual() {
+ let horaHtml= document.getElementById("horaActual");
 
+    
+    let hora=new Date();
+    let horaH=hora.getHours()
+    let horaM=hora.getMinutes()
+    let horaS=hora.getSeconds()
+
+    horaHtml.innerHTML=horaH+": "+horaM+": "+horaS
+}    
+  MhoraActual()  
+
+setInterval(MhoraActual, 1000)
