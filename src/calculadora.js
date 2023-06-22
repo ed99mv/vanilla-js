36f727e04c6ce681e8d43f43c29d4e3f26332895
Edward Medina <medinaedward988@gmail.comm>
2023-06-22 15:47:44 -0600

    ejercicio5 terminado

---
+++ src/calculadora.js
@@ -0,0 +1,16 @@
+
+const calculadora={
+    sumar: (valor1, valor2)=> {
+        return valor1 + valor2;
+    },
+    restar: (valor1, valor2)=> {
+        return valor1 - valor2
+    },
+    multiplicar:  (valor1, valor2)=>{
+        return valor1 * valor2
+    },
+    dividir:  (valor1,valor2)=> {
+        return valor1 / valor2
+    },
+}
+ export{calculadora}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 // Inserte el código aquí
+import { calculadora } from "./calculadora.js";
+console.log(calculadora);
+
+
+
+
+
+function calcular(operacion, numero1, numero2) {
+    if (operacion=="suma") {
+        
+        console.log(calculadora.sumar(numero1,numero2))
+    }else{
+        if (operacion=="resta") {
+            console.log(calculadora.restar(numero1,numero2))
+        }else{
+            if (operacion=="multiplicar") {
+                console.log(calculadora.multiplicar(numero1,numero2));
+            }else{
+                if (operacion=="dividir") {
+                    console.log(calculadora.dividir(numero1,numero2));
+                }
+            }
+        }
+    }   
+    
+
+}
+calcular("suma",2,2)
+calcular("resta",2,2)
+calcular("multiplicar",2,2)
+calcular("dividir",2,2)
 
