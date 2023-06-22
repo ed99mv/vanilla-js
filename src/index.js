91307d1fa59f80e49c9dd1b88490335909be8ab9
Edward Medina <medinaedward988@gmail.comm>
2023-06-22 13:09:52 -0600

    actualizar 1pm

--- src/index.js
+++ src/index.js
@@ -13,3 +13,49 @@ function MhoraActual() {
   MhoraActual()  
 
 setInterval(MhoraActual, 1000)
+
+
+var divUsers=document.getElementById("div_users")
+fetch("http://localhost:3000/users")
+  .then((data=>data.json()))
+  .then(users=>{
+    
+    console.log(users);
+    
+
+  
+
+    
+    /*usuarios.map(function (user, i) {
+      let nombre = document.createElement("h2");
+      nombre.innerHTML=i+" "+ user.name+" "+user.birth;
+      divUsers.appendChild(nombre);
+
+
+    })*/
+    
+})
+.catch((error)=>{
+  alert("Error en la petición")
+})
+
+/*var datos={
+  "mohademago": {
+    "id": 4294825,
+    "name": "mohademago",
+    "profileIconId": 1594,
+    "revisionDate": 1491930966000,
+    "summonerLevel": 30
+  }
+};
+
+
+
+[
+  {"id":1,"name":"Maria",
+  "birth":1990
+},
+  {"id":2,
+  "name":"Luis",
+  "birth":1980
+}]*/
